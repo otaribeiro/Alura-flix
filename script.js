ff655af82279b5115dfaab9ee7d273a2bfe3deb5
Otávio Ribeiro <ribeiro.otavio93@gmail.com>
2023-02-03 16:58:55 -0300

    Otaflix

---
+++ script.js
@@ -0,0 +1,29 @@
+var listaFilmes = []
+var nomeFilmes = [];
+
+const container = document.querySelector('.content-container');
+
+listaFilmes[0] = "https://img.elo7.com.br/product/original/269274A/poster-o-senhor-dos-aneis-a-sociedade-do-anel-lo04-90x60-cm-presente-geek.jpg";
+nomeFilmes[0] = "O senhor dos anéis: A sociedade do Anel";
+
+listaFilmes[1] = "https://img.elo7.com.br/product/original/26927C9/poster-o-senhor-dos-aneis-as-duas-torres-lo01-tam-90x60-cm-presente-geek.jpg";
+nomeFilmes[1] = "O senhor dos anéis: As duas torres";
+
+listaFilmes[2] = "https://img.elo7.com.br/product/original/2692931/big-poster-o-senhor-dos-aneis-o-retorno-do-rei-lo07-90x60-cm-poster.jpg";
+nomeFilmes[2] = "O senhor dos anéis: O retorno do rei";
+
+listaFilmes[3] = "https://img.elo7.com.br/product/zoom/268E0B6/big-poster-o-hobbit-uma-jornada-inesperada-lo07-tam-90x60-cm-poster.jpg";
+nomeFilmes[3] = "O hobbit: Uma jornada inesperada";
+
+listaFilmes[4] = "https://img.elo7.com.br/product/zoom/2692268/big-poster-o-hobbit-a-desolacao-de-smaug-lo07-tam-90x60-cm-o-hobbit.jpg";
+nomeFilmes[4]= "O hobbit: A desolação de Smaug"
+
+for (var i = 0; i < listaFilmes.length; i++) {
+    container.innerHTML =
+      container.innerHTML +
+      '<div class="image-container"><img src=' +
+      listaFilmes[i] +
+      '><span class="image-name">' +
+      nomeFilmes[i] +
+      '</span></div>';
+  }
